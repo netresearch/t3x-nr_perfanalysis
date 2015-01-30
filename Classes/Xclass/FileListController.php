e3fcd5ae40887e5daeaaf99853c5f7e366e21546
Christian Weiske <christian.weiske@netresearch.de>
2015-01-30 11:05:09 +0100

    AGP-1524: show times in FileListController

---
+++ Classes/Xclass/FileListController.php
@@ -0,0 +1,42 @@
+<?php
+declare(encoding = 'UTF-8');
+/**
+ * Part of nr_perfanalysis
+ *
+ * PHP version 5
+ *
+ * @category Netresearch
+ * @package  Perfanalysis
+ * @author   Christian Weiske <christian.weiske@netresearch.de>
+ * @license  http://www.gnu.org/licenses/agpl-3.0.html AGPL v3 or later
+ * @link     http://www.netresearch.de/
+ */
+namespace Netresearch\NrPerfanalysis\Xclass;
+use \Netresearch\NrPerfanalysis\HtmlRenderer;
+
+/**
+ * Counts events and times
+ *
+ * @category Netresearch
+ * @package  Perfanalysis
+ * @author   Christian Weiske <christian.weiske@netresearch.de>
+ * @license  http://www.gnu.org/licenses/agpl-3.0.html AGPL v3 or later
+ * @link     http://www.netresearch.de/
+ */
+class FileListController
+    extends \TYPO3\CMS\Filelist\Controller\FileListController
+{
+    /**
+     * Outputting the accumulated content to screen
+     *
+     * @return void
+     */
+    public function printContent()
+    {
+        parent::printContent();
+
+        $hr = new HtmlRenderer();
+        echo $hr->genHtml();
+    }    
+}
+?>
--- ext_localconf.php
+++ ext_localconf.php
@@ -13,17 +13,24 @@
  */
 defined('TYPO3_MODE') or die('Access denied.');
 
-if (TYPO3_MODE == 'FE') {
-    $extConf = unserialize(
-        $GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['nr_perfanalysis']
-    );
-    if ($extConf['enable']) {
-        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_db.php']
-            ['queryProcessors'][]
-                = 'Netresearch\NrPerfanalysis\QueryHooker';
+$extConf = unserialize(
+    $GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['nr_perfanalysis']
+);
+if ($extConf['enable']) {
+    //do nothing
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_db.php']
+        ['queryProcessors'][]
+        = 'Netresearch\NrPerfanalysis\QueryHooker';
+    if (TYPO3_MODE == 'FE') {
         $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']
             ['contentPostProc-output'][]
-                = 'Netresearch\NrPerfanalysis\HtmlRenderer->contentPostProcOutput';
+            = 'Netresearch\NrPerfanalysis\HtmlRenderer->contentPostProcOutput';
+    } else if (TYPO3_MODE == 'BE') {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']
+            ['TYPO3\\CMS\\Filelist\\Controller\\FileListController']
+            = array(
+                'className' => 'Netresearch\\NrPerfanalysis\\Xclass\\FileListController'
+            );
     }
 }
 ?>
