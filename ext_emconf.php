8e50aee43636bc4aa6cc7bd88f2b9a6df924bd6d
Axel Kummer <axel.kummer@netresearch.de>
2017-10-12 10:18:06 +0200

    Version: 1.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,19 +1,30 @@
 <?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "nr_perfanalysis".
+ *
+ * Auto generated 12-10-2017 10:18
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
 $EM_CONF[$_EXTKEY] = array(
-    'title' => 'Performance analysis',
-    'description' => 'Show SQL statistics for frontend rendering directly in the frontend.',
-    'category' => 'frontend',
-    'author' => 'Christian Weiske',
-    'author_company' => 'Netresearch GmbH & Co.KG',
-    'author_email' => 'typo3@cweiske.de',
-    'dependencies' => '',
-    'state' => 'stable',
-    'clearCacheOnLoad' => '1',
-    'version' => '1.0.0',
-    'constraints' => array(
-        'depends' => array(
-            'typo3' => '6.2.0-7.99.99',
-        )
-    )
+	'title' => 'Performance analysis',
+	'description' => 'Show SQL statistics for frontend rendering directly in the frontend.',
+	'category' => 'frontend',
+	'author' => 'Christian Weiske',
+	'author_company' => 'Netresearch GmbH & Co.KG',
+	'author_email' => 'typo3@cweiske.de',
+	'dependencies' => '',
+	'state' => 'stable',
+	'clearCacheOnLoad' => 1,
+	'version' => '1.1.0',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '6.2.0-7.99.99',
+		),
+	),
 );
 ?>
